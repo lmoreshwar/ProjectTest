86653ac1eff7d4ecdd2c8e11f93c8b918e91d378
L Moreshwar <E002985@cigniti.com>
2017-07-11 16:13:33 +0530

    code commited

---
+++ project3.java
@@ -0,0 +1,13 @@
+package Project2;
+
+public class project3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println(" This is a testing project ");
+		
+	}
+
+}
